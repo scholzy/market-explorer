7fecbae50240b6f4734eb10bf0ef0394f121ea3f
Michael Scholz <m@scholz.moe>
2023-12-26 18:48:49 +0000

    Add linter and enable debug

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
+import os
+
 from arcticdb import Arctic
+from dotenv import load_dotenv
 from flask import Flask
 
+# Initialise the Flask app
 app = Flask(__name__)
 
+# And the fast tabular database (locally, for now)
 db = Arctic("lmdb://./db")
 
-@app.route('/')
-def index():
-    db.create_library('stonks')
-    return 'Hello World!'
+# Load the environment variables if present
+load_dotenv()
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route("/")
+def index():
+    db.create_library("stonks")
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    DEBUG_MODE = os.environ.get("FLASK_ENV") == "development"
+    app.run(debug=DEBUG_MODE)
