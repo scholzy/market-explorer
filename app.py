7722dcb6ff3221adf4c9f7b901df9805fce51c8d
Michael Scholz <m@scholz.moe>
2023-12-27 10:46:03 +0000

    Embed Dash in Flask

--- app.py
+++ app.py
@@ -3,8 +3,11 @@
 import os
 
 from arcticdb import Arctic
+import dash
 from dotenv import load_dotenv
 from flask import Flask
+
+from app.layout import layout
 
 # Initialise the Flask app
 app = Flask(__name__)
@@ -16,11 +19,16 @@ db = Arctic("lmdb://./db")
 load_dotenv()
 
 
+# Define the base route
 @app.route("/")
 def index():
     db.create_library("stonks")
     return "Hello World!"
 
+
+# Initialise the Dash app -- reusing the Flask server to host it
+dash_app = dash.Dash(server=app, url_base_pathname="/dashboard/")  # type: ignore
+dash_app.layout = layout()
 
 if __name__ == "__main__":
     DEBUG_MODE = os.environ.get("FLASK_ENV") == "development"
---
+++ app/layout.py
@@ -0,0 +1,5 @@
+from dash import html
+
+
+def layout():
+    return html.Div([html.H1("Hello World"), html.P("This is a paragraph")])
